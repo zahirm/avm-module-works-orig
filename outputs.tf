9c0ac5d24d3a17b63d75ab50c62852e958ed08bc
Zahir Mosaib <zahirm@users.noreply.github.com>
2026-03-25 11:36:21 -0400

    chore(terraform): sync storageaccount module outputs from upstream AVM source

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,35 @@
-output "name" {
-  description = "The name of the Network Security Group resource"
-  value       = azurerm_network_security_group.this.name
+output "storage_account_id" {
+  description = "The resource ID of the Storage Account."
+  value       = azurerm_storage_account.this.id
 }
 
-output "resource_id" {
-  description = "The id of the Network Security Group resource"
-  value       = azurerm_network_security_group.this.id
+output "storage_account_name" {
+  description = "The name of the Storage Account."
+  value       = azurerm_storage_account.this.name
 }
 
-output "security_rules" {
-  description = "The Network Security Group Rules"
-  value = {
-    for key, rule in azurerm_network_security_rule.this : key => merge(rule, {
-      destination_address_prefixes               = rule.destination_address_prefixes == null ? [] : rule.destination_address_prefixes
-      destination_application_security_group_ids = rule.destination_application_security_group_ids == null ? [] : rule.destination_application_security_group_ids
-      destination_port_ranges                    = rule.destination_port_ranges == null ? [] : rule.destination_port_ranges
-      source_address_prefixes                    = rule.source_address_prefixes == null ? [] : rule.source_address_prefixes
-      source_application_security_group_ids      = rule.source_application_security_group_ids == null ? [] : rule.source_application_security_group_ids
-      source_port_ranges                         = rule.source_port_ranges == null ? [] : rule.source_port_ranges
-    })
-  }
+output "storage_account_primary_web_endpoint" {
+  description = "The URL of the primary web endpoint."
+  value       = azurerm_storage_account.this.primary_web_endpoint
+}
+
+output "storage_account_primary_connection_string" {
+  description = "The connection string for the primary location."
+  value       = azurerm_storage_account.this.primary_connection_string
+}
+
+output "storage_account_primary_blob_endpoint" {
+  description = "The blob endpoint for the primary location."
+  value       = azurerm_storage_account.this.primary_blob_endpoint
+}
+
+output "storage_account_primary_access_key" {
+  description = "The primary access key for the storage account."
+  value       = azurerm_storage_account.this.primary_access_key
+}
+
+output "storage_account_secondary_access_key" {
+  description = "The secondary access key for the storage account."
+  value       = azurerm_storage_account.this.secondary_access_key
+  sensitive   = true
 }
